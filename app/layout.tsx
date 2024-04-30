920d59f9f96be364326c5b985fe2ad2198552836
Andrey <andreymatheus92@gmail.com>
2024-04-30 19:31:50 -0300

    feat: add initial header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>layout</h1>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Header from "./_components/header";
+
 const Home = () => {
-  return <div className="mx-6 bg-red-500 py-5"></div>;
+  return (
+    <Header />
+  )
 };
 
 export default Home;
