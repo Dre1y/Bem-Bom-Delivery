ce4ce9503c27dd9653122a4af0b9b291f7854fc9
Andrey <andreymatheus92@gmail.com>
2024-05-01 17:06:59 -0300

    style: update category item styles

--- app/_components/category-item.tsx
+++ app/_components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({category}: CategoryItemProps) => {
     return ( 
-        <div className="flex items-center gap-3 bg-white px-4 py-3 shadow-md rounded-full w-full">
+        <div className="flex justify-center items-center gap-3 bg-white px-4 py-3 shadow-md rounded-full w-full">
             <Image src={category.imageUrl} alt={category.name} width={30} height={30}
             />
 
