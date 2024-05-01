481c5ccb5f0f583ed77a07a44fa6ab27e26fb7ee
Andrey <andreymatheus92@gmail.com>
2024-05-01 13:24:38 -0300

    feat: update promo banner file

--- app/_components/promo-banner.tsx
+++ app/_components/promo-banner.tsx
@@ -1,9 +1,5 @@
 import Image, { ImageProps } from "next/image";
 
-interface PromoBannerProps {
-
-}
-
 const PromoBanner = (props: ImageProps) => {
     return ( 
         <Image height={0} width={0} className="w-full h-auto object-contain" sizes="100vw" quality={100} {...props}/>
