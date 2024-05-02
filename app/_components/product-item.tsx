47b46e0492d5b740055ea37f4634b6ae5564d48b
Andrey <andreymatheus92@gmail.com>
2024-05-02 19:39:12 -0300

    feat: add link to product item component

--- app/_components/product-item.tsx
+++ app/_components/product-item.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import { Prisma } from "@prisma/client";
 import Image from "next/image";
 import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
 import { ArrowDownIcon } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
     product: Prisma.ProductGetPayload<{
@@ -17,8 +20,9 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
     return (
-        <div className="space-y-2 w-[150px] min-w-[150px]">
+        <Link className="w-[150px] min-w-[150px]" href={`/products/${product.id}`}>
 
+            <div className="w-full space-y-2">
             <div className="h-[150px] w-full relative">
                 <Image src={product.imageUrl} alt={product.name} fill className="object-cover rounded-lg shadow-md"
                 />
@@ -30,26 +34,27 @@ const ProductItem = ({ product }: ProductItemProps) => {
                     </div>
                 )}
             </div>
-            
-            <div>
-                <h2 className="text-sm truncate">{product.name}</h2>
-                <div className="flex gap-1 items-center">
-                    <h3 className="font-semibold">
-                        {formatCurrency(calculateProductTotalPrice(product))}
-                    </h3>
-                    {product.discountPercentage > 0 && (
-                        <span className="text-muted-foreground line-through text-xs">
-                            {formatCurrency(Number(product.price))}
-                        </span>
-                    )}
-                </div>
 
-                <span className="block text-muted-foreground text-xs">
-                    {product.restaurant.name}
-                    </span>
-            </div>
-
+        <div>
+            <h2 className="text-sm truncate">{product.name}</h2>
+            <div className="flex gap-1 items-center">
+                <h3 className="font-semibold">
+                {formatCurrency(calculateProductTotalPrice(product))}
+                </h3>
+                {product.discountPercentage > 0 && (
+                <span className="text-muted-foreground line-through text-xs">
+                {formatCurrency(Number(product.price))}
+                </span>
+            )}
         </div>
+
+        <span className="block text-muted-foreground text-xs">
+            {product.restaurant.name}
+        </span>
+        </div>
+
+            </div>
+        </Link>
     )
 }
  
